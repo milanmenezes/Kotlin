f245c9019a7ebf2a8c57318e2d084e1ac5a9d9d0
milanmenezes <milanmenezes@gmail.com>
2017-07-01 14:55:43 +0530

    First commit

---
+++ app/src/main/java/me/milanmenezes/kotlin/MainActivity.kt
@@ -0,0 +1,22 @@
+package me.milanmenezes.kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val nam = findViewById(R.id.editText1) as EditText
+
+        val btn = findViewById(R.id.button1) as Button
+        btn.setOnClickListener {
+            toast("Hello ${nam.text}");
+        }
+    }
+}
