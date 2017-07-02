a9135d3435bad05d8c4e7b7344c883472b3379f4
milanmenezes <milanmenezes@gmail.com>
2017-07-02 18:17:06 +0530

    Calculator

--- app/src/main/java/me/milanmenezes/kotlin/MainActivity.kt
+++ app/src/main/java/me/milanmenezes/kotlin/MainActivity.kt
@@ -9,15 +9,45 @@ import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
+    fun findsum(a:Int, b:Int):Int{
+        return a+b
+    }
+
+    fun findsub(a:Int, b:Int):Int{
+        return a-b
+    }
+
+    fun findmul(a:Int, b:Int):Int{
+        return a*b
+    }
+
+    fun finddiv(a:Int, b:Int):Float{
+        return (a.toFloat()/b)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val nam = editText1
 
-//        val btn = button1;
         button1.setOnClickListener {
-            toast("Hello ${editText1.text}!");
+            toast("The sum is "+findsum(editText1.text.toString().toInt(),editText2.text.toString().toInt())+"!")
+
+        }
+
+        button2.setOnClickListener {
+            toast("The remainder is "+findsub(editText1.text.toString().toInt(),editText2.text.toString().toInt())+"!")
+//
+        }
+
+        button3.setOnClickListener {
+            toast("The product is "+findmul(editText1.text.toString().toInt(),editText2.text.toString().toInt())+"!")
+//
+        }
+
+        button4.setOnClickListener {
+            toast("The quotient is "+finddiv(editText1.text.toString().toInt(),editText2.text.toString().toInt())+"!")
+//
         }
     }
 }
